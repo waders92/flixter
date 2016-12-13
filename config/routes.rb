4d3a16aa4dd2f79b98244248311e5f8c15827279
waders92 <wade.carlson@pga.com>
2016-12-13 16:53:59 -0600

    allowed course creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
   
   
   
