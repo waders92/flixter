49173c5ff6acb04a1e9cf971cf3a13181ce33369
waders92 <wade.carlson@pga.com>
2016-12-20 10:52:09 -0600

    Error with mini-magick gem

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,11 +2,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-   storage :fog
+   storage :file
+   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+   process resize_to_fill: [800, 350]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
