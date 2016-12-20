51d2f1dcafc616ef250fff9a3af99017391c5b9c
waders92 <wade.carlson@pga.com>
2016-12-20 13:19:29 -0600

    logic for storage files added

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -5,8 +5,12 @@ class VideoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+
+  if Rails.env = "development"
+   storage :file
+ else
    storage :aws
+ end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
