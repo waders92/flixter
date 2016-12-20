ae974b62ea4fc5bee7bf072e0df9b8927526d90a
waders92 <wade.carlson@pga.com>
2016-12-20 12:32:02 -0600

    added video upload capability

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,11 +1,11 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class VideoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-   include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  #  storage :file
+  # storage :file
    storage :aws
 
   # Override the directory where uploaded files will be stored.
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-   process resize_to_fill: [900, 350]
+  #   process resize_to_fit: [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216222733) do
+ActiveRecord::Schema.define(version: 20161220182056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20161216222733) do
     t.integer  "section_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "video"
   end
 
   add_index "lessons", ["section_id"], name: "index_lessons_on_section_id", using: :btree
