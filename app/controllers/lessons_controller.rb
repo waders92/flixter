e2d775f43633f9b8d49f469ede1e205786f9a86e
waders92 <wade.carlson@pga.com>
2016-12-21 10:16:36 -0600

    added authentication to view lessons - you have to be enrolled

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,13 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user!
   def show
+    if !current_user.enrolled_in?(current_lesson.section.course) && current_lesson.section.course.user != current_user
+        redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled in this course!'
+        end
   end
 
-  
-  
+
+
   private
   helper_method :current_lesson
   def current_lesson
